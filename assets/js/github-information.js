8996d12136ed3628f53e5c62e3aa78f6a58beb34
Wicky <wicky.akloe@gmail.com>
2019-04-20 12:20:26 +0000

    added gif and rendered github user information

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,21 @@
+function userInformationHTML(user){
+    return `
+    <h2>${user.name}
+        <span class="small-name">
+            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+        </span>
+    </h2>
+    <div class="gh-content">
+        <div class="gh-avatar">
+            <a href="${user.html_url}" target="_blank">
+                <img src="${user.avatar_url}" widht="80" height="80" alt="${user.login}" />
+            </a>
+        </div>
+        <p>Follower: ${user.followers} - Following ${user.following} <br. Repos: ${user.public_repos}</p>
+    </div>`
+}
+
+
 function fetchGitHubInformation(event){
     
     var username = $("#gh-username").val();
@@ -8,7 +26,43 @@ function fetchGitHubInformation(event){
     
     $("#gh-user-data").html(
         `<div id=loader>
-            <img src="assets/css/loader.gif" alt="loading..." />  
+            <img src="assets/css/giphy.gif" alt="loading..." />  
         </div>`);
+        
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+        ).then(
+            function(response){
+                var userData = response;
+                $("#gh-user-data").html(userInformationHTML(userData));
+            }, function(errorResponse){
+                if (errorResponse.status === 404){
+                    $("#gh-user-data").html(`
+                    <h2>No info found for user ${username}</h2>`);
+                } else {
+                    console.log(errorResponse);
+                    $("gh-user-data").html(
+                        `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                }
+            });
     
 }
+
+/*JQuery Promises
+in example:
+When: 
+    You complete this course
+Then: 
+    i will buy you diner
+
+in JQuery code:
+
+$.when(
+    // we have got a response from the GitHub API
+).then (
+    // function to display it in the gh-user-data div
+    // unless we get an error!
+)
+    
+    
+*/
