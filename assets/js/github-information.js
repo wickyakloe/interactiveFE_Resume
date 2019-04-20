c265b84692bef11e95172673eabee52e1fa6f2a4
Wicky <wicky.akloe@gmail.com>
2019-04-20 12:51:17 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -40,6 +40,8 @@ function repoInformationHTML(repos){
 
 
 function fetchGitHubInformation(event){
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     
     var username = $("#gh-username").val();
     if (!username){
@@ -73,6 +75,8 @@ function fetchGitHubInformation(event){
             });
     
 }
+
+$(document).ready(fetchGitHubInformation());
 
 /*JQuery Promises
 in example:
